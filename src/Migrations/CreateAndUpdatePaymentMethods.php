6a94c968cf257a43bc54cd429bb601663a84dcb4
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2021-01-12 17:22:49 +0530

    Update CreateAndUpdatePaymentMethods.php

--- src/Migrations/CreateAndUpdatePaymentMethods.php
+++ src/Migrations/CreateAndUpdatePaymentMethods.php
@@ -56,6 +56,7 @@ class CreateAndUpdatePaymentMethods
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_CC', 'Novalnet Credit/Debit Cards');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_SEPA', 'Novalnet Direct Debit SEPA');
         $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_INVOICE', 'Novalnet Invoice');
+        $this->createNovalnetPaymentMethodByPaymentKey('NOVALNET_PAYPAL', 'Novalnet PayPal');
     }
 
     /**
